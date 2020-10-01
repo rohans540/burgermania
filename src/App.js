cebcba2b85e4c72deb162026e655c36416377010
Rohan <rohans540@gmail.com>
2020-10-01 23:43:35 +0530

    Burger component created with styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
-import Layout from './components/layout/layout';
+import Layout from './components/layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p>Testing the layout component</p>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Auxi';
+
+
+class BurgerBuilder extends Component {
+
+    render() {
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Build controls</div>
+            </Aux>
+        );
+    }
+};
+
+export default BurgerBuilder;
